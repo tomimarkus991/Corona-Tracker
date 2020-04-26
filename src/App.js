4149969c2487096d1e1f8d32041bc917744bbe0a
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-04-26 14:49:04 +0300

    Added parser babel-eslint to eslintrc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
-import { fetchDataGlobal, fetchDailyDataCountries, fetchDataCountry } from '../src/api';
+import { fetchDataGlobal, fetchDailyDataCountries } from '../src/api';
 
 import coronaImage from './assets/image.png';
 
@@ -9,27 +9,45 @@ class App extends React.Component {
 	state = {
 		data: {},
 		data2: {},
-		country: ''
-	}
+		country: '',
+	};
 	async componentDidMount() {
 		const fetchedDataGlobal = await fetchDataGlobal();
-		this.setState({ data: fetchedDataGlobal })
+		this.setState({ data: fetchedDataGlobal });
 	}
-	handleCountryChange = async country => {
+	handleCountryChange = async (country) => {
 		const fetchedDataGlobal = await fetchDataGlobal(country);
 		const fetchedDailyDataCountries = await fetchDailyDataCountries(country);
-		this.setState({ data: fetchedDataGlobal, data2: fetchedDailyDataCountries, country: country })
-	}
+		this.setState({
+			data: fetchedDataGlobal,
+			data2: fetchedDailyDataCountries,
+			country: country,
+		});
+	};
 	render() {
 		const { data, data2, country } = this.state;
 		return (
 			<div className="columns is-centered has-text-centered is-multiline">
-				<div className="column is-two-thirds is-centered"><a href="https://covid19api.com/" target="_blank"><img className="covidImage" src={coronaImage}></img></a></div>
-				<div className="shit">
-				<div className="column is-full"><Cards data={data}></Cards></div>
-				<div className="countryPicker column is-full"><CountryPicker handleCountryChange={this.handleCountryChange}></CountryPicker></div>
+				<div className="column is-two-thirds is-centered">
+					<a
+						href="https://covid19api.com/"
+						rel="noopener noreferrer"
+						target="_blank">
+						<img className="covidImage" src={coronaImage}></img>
+					</a>
 				</div>
-				<div className="column is-full"><Chart data2={data2} country={country}></Chart></div>
+				<div className="shit">
+					<div className="column is-full">
+						<Cards data={data}></Cards>
+					</div>
+					<div className="countryPicker column is-full">
+						<CountryPicker
+							handleCountryChange={this.handleCountryChange}></CountryPicker>
+					</div>
+				</div>
+				<div className="column is-full">
+					<Chart data2={data2} country={country}></Chart>
+				</div>
 			</div>
 		);
 	}
